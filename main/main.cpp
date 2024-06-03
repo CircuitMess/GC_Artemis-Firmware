8fab35a2acd231c08e6e0a128682ac5b702ffed6
Emil Gajšak <emil.gajsak@gmail.com>
2024-06-03 11:07:53 +0200

    Changed intro startup sound

--- main/main.cpp
+++ main/main.cpp
@@ -150,17 +150,18 @@ void init(){
 
 	if(settings->get().notificationSounds){
 		audio->play({
-							Chirp{ .startFreq = 250, .endFreq = 250, .duration = 500 },
-							Chirp{ .startFreq = 0, .endFreq = 0, .duration = 200 },
-							Chirp{ .startFreq = 500, .endFreq = 500, .duration = 500 },
-							Chirp{ .startFreq = 0, .endFreq = 0, .duration = 200 },
-							Chirp{ .startFreq = 1000, .endFreq = 1000, .duration = 500 }
-					});
-
-		vTaskDelay(2500);
-
-		audio->play({
-							Chirp{ .startFreq = 125, .endFreq = 125, .duration = 1000 }
+							Chirp{ .startFreq = 300, .endFreq = 600, .duration = 250 },
+							Chirp{ .startFreq = 600, .endFreq = 300, .duration = 250 },
+							Chirp{ .startFreq = 0, .endFreq = 0, .duration = 100 },
+							Chirp{ .startFreq = 600, .endFreq = 600, .duration = 75 },
+							Chirp{ .startFreq = 0, .endFreq = 0, .duration = 75 },
+							Chirp{ .startFreq = 500, .endFreq = 500, .duration = 75 },
+							Chirp{ .startFreq = 0, .endFreq = 0, .duration = 75 },
+							Chirp{ .startFreq = 500, .endFreq = 500, .duration = 75 },
+							Chirp{ .startFreq = 0, .endFreq = 0, .duration = 75 },
+							Chirp{ .startFreq = 500, .endFreq = 500, .duration = 75 },
+							Chirp{ .startFreq = 0, .endFreq = 0, .duration = 75 },
+							Chirp{ .startFreq = 850, .endFreq = 850, .duration = 75 }
 					});
 	}
 
